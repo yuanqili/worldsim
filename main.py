9760250dd8617a09273053ad3eb8cea25e4d7ebe
yuanqili <yuanqili1018@gmail.com>
2023-02-19 15:54:11 +0800

    Randomly generates some Human agent in the map

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
+import random
 
+from taxsim.agent import Human
 from taxsim.driver import Driver
 
 if __name__ == '__main__':
-    driver = Driver(40, 120)
+    driver = Driver(40, 80)
+    agents = []
+    for _ in range(20):
+        row = random.randint(0, driver.world.rows - 1)
+        col = random.randint(0, driver.world.cols - 1)
+        agent = Human()
+        driver.world.cells[row][col].add_agent(agent)
+        agents.append(agent)
+    print(agents)
     driver.run(0.5)
