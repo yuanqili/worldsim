a52e569041f69b18e0ced5731179e8686cd8f2ff
yuanqili <yuanqili1018@gmail.com>
2023-02-19 15:54:00 +0800

    Updates Food and adds Gold

--- taxsim/item.py
+++ taxsim/item.py
@@ -1,20 +1,40 @@
-class Item:
+from termcolor import colored
+import numpy as np
 
+
+class Item:
     item_name = 'Generic item'
 
-    def __init__(self, cell):
-        self.cell = cell
+    def __init__(self):
+        self.cell = None
 
     def tick(self):
         return
 
 
 class Food(Item):
-
     item_name = 'Food'
 
-    def __init__(self, cell=None):
-        super().__init__(cell)
+    def __init__(self, nutrition=None):
+        super().__init__()
+        self.nutrition = nutrition or np.random.normal(10, 5)
 
     def tick(self):
         return
+
+    def __repr__(self):
+        return colored('*', 'white', 'on_yellow')
+
+
+class Gold(Item):
+    item_name = 'Gold'
+
+    def __init__(self, value=None):
+        super().__init__()
+        self.value = value or np.random.normal(20, 3)
+
+    def tick(self):
+        return
+
+    def __repr__(self):
+        return colored('$', 'white', 'on_yellow')
